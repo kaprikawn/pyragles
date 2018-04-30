5340ee88334889c7b5f00d4b1b8acad3137386da
kaprikawn <wayne@kaprikawn.co.uk>
2018-04-30 20:28:05 +0100

    rudimentary camera movement added

--- src/camera.cpp
+++ src/camera.cpp
@@ -11,10 +11,26 @@ Camera::Camera( int windowWidth, int windowHeight ) {
   
   projection_ = glm::perspective( glm::radians( 60.0f ), aspect, 1.0f, 65.0f );
   
+  position_ = { 0.0f, 0.0f, 4.0f };
+  
   view_ = glm::lookAt(
-      glm::vec3( 0.0f, 0.0f, 4.0f )
-    , glm::vec3( 0, 0, -10.0f )
+      position_
+    , glm::vec3( 0, 0, -50.0f )
     , glm::vec3( 0, 1, 0 )
   );
 }
 
+void Camera::update( std::shared_ptr<glm::vec3> shipPosition, GLfloat dt ) {
+  glm::vec3 velocity;
+  velocity.x = shipPosition -> x - position_.x;
+  velocity.y = shipPosition -> y - position_.y;
+  velocity.z = 0;
+  
+  position_ += velocity * dt * 5.0f;
+  
+  view_ = glm::lookAt(
+      position_
+    , glm::vec3( 0, 0, -10.0f )
+    , glm::vec3( 0, 1, 0 )
+  );
+}
--- src/camera.hpp
+++ src/camera.hpp
@@ -3,6 +3,7 @@
 
 #include <GLES2/gl2.h>
 #include <glm/glm.hpp>
+#include <memory>
 
 class Camera {
   private:
@@ -10,19 +11,23 @@ class Camera {
     glm::mat4   projection_;
     glm::mat4   view_;
     
+    glm::vec3   position_;
+    
     GLsizei windowWidth_;
     GLsizei windowHeight_;
     
   public:
     Camera( int windowWidth, int windowHeight );
     ~Camera(){}
+    
+    void update( std::shared_ptr<glm::vec3> shipPosition, GLfloat dt );
   
-  glm::mat4 viewProjectionMatrix() {
-    return projection_ * view_;
-  }
-  
-  GLsizei windowWidth()   { return windowWidth_; }
-  GLsizei windowHeight()  { return windowHeight_; }
+    glm::mat4 viewProjectionMatrix() {
+      return projection_ * view_;
+    }
+    
+    GLsizei windowWidth()   { return windowWidth_; }
+    GLsizei windowHeight()  { return windowHeight_; }
 };
 
 typedef Camera TheCamera;
