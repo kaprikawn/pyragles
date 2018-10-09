9c9a31ab2893f424aeee9b6c7ed67128b0e7688c
Wayne Briggs <wayne@kaprikawn.co.uk>
2018-10-09 16:06:14 +0100

    glm fix for v0.2.3

--- src/glObject.hpp
+++ src/glObject.hpp
@@ -29,11 +29,11 @@ class GlObject {
     
     GLvoid*     colourOffset_;
     
-    glm::mat4   projection_;
-    glm::mat4   view_;
-    glm::mat4   model_;
-    glm::mat4   rotation_;
-    glm::mat4   mvp_;
+    glm::mat4   projection_ = glm::mat4( 1.0f );
+    glm::mat4   view_       = glm::mat4( 1.0f );
+    glm::mat4   model_      = glm::mat4( 1.0f );
+    glm::mat4   rotation_   = glm::mat4( 1.0f );
+    glm::mat4   mvp_        = glm::mat4( 1.0f );
     
     Vector      velocity_;
     Position    position_;
