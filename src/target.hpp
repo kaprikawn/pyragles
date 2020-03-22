6bad36acc4b30afa2a16816fcbe02b3da371e18b
Wayne Briggs <wayne@kaprikawn.co.uk>
2020-03-22 17:30:37 +0000

    target working

---
+++ src/target.hpp
@@ -0,0 +1,22 @@
+#ifndef TARGET_HPP
+#define TARGET_HPP
+
+#include <vector>
+#include "gameObject.hpp"
+
+class Target : public GameObject {
+  
+  private :
+    
+    std::vector<float> vertexData_;
+    
+  public :
+    Target();
+    virtual ~Target();
+    
+    virtual void update( float dt, glm::vec3 shipPosition, float shipAngleX, float shipAngleY );
+    virtual void render( glm::mat4 viewProjectionMatrix );
+    
+};
+    
+#endif //TARGET_HPP
