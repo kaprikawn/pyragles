4c3da0f4ce9382638bfca246c91636506badfd24
Wayne Briggs <wayne@kaprikawn.co.uk>
2022-06-10 17:28:28 +0100

    Take out debug camera movement

--- src/camera.hpp
+++ src/camera.hpp
@@ -7,18 +7,6 @@
 
 void caculate_camera( GameState* game_state, GameInput* game_input, f32 dt ) {
   
-  if( game_input -> arrow_up_held ) {
-    game_state -> eye.y -= ( 10.0f * dt );
-  } else if( game_input -> arrow_down_held ) {
-    game_state -> eye.y += ( 10.0f * dt );
-  }
-  
-  if( game_input -> pg_up_held ) {
-    game_state -> look_at.z += ( 10.0f * dt );
-  } else if( game_input -> pg_down_held ) {
-    game_state -> look_at.z -= ( 10.0f * dt );
-  }
-  
   f32 v[ 16 ]    = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };    // view
   
   populate_view_matrix( &v[ 0 ], game_state -> eye, game_state -> look_at );
